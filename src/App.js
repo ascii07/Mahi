85d298d24a57444529e3d80ab5bc2ae7f47ee054
ascii07 <104676725+ascii07@users.noreply.github.com>
2024-06-19 04:22:08 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Pages/Home';
+import Legalities from './Pages/Legalities';
+
+import HowToPlay from './Pages/Howtoplay';
+import FAQ from './Pages/Contact';
+import UserSays from './Pages/Usersays';
+import PrivacyPolicy from './Pages/privacy_policy';
+import Disclamier from './Pages/Disclamier';
+import ResponsibleGaming from './Pages/ResponsibleGaming';
+
+
+import TermsAndCondition from './Pages/TermsAndCondition';
+
+
+import Footer from './Components/Footer/Footer';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        
+        
+        
+        
+        
+        <Route path="/Howtoplay" element={<HowToPlay />} />
+        <Route path="/faqs" element={<FAQ />} />
+        <Route path="/user-says" element={<UserSays />} />
+        <Route path="/privacy_policy" element={<PrivacyPolicy />} />
+        <Route path="/Disclamier" element={<Disclamier />} />
+        <Route path="/TermsAndCondition" element={<TermsAndCondition />} />
+        <Route path="/Legalities" element={<Legalities />} />
+        <Route path="/ResponsibleGaming" element={<ResponsibleGaming/>} />
+
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
